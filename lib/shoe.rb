fc14ae886a4d2ddb3563ecb37053b60323360721
Miriam Castillo <castillo.m.mc@gmail.com>
2018-07-31 06:37:01 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 class Shoe
 
   attr_accessor :brand, :color, :size, :material, :condition
-  
+
 
   def initialize(cobble)
     @cobble = cobble
   end
 
-def turn_page
+def cobble
   puts "Flipping the page...wow, you read fast!"
 end
 end
